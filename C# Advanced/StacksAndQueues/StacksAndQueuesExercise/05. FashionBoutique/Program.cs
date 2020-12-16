40e70e287cd1459be216ebe3ef995677ebaeb13b
Teodor Yanchev <teo.yanchev93@gmail.com>
2020-12-16 13:30:26 +0200

    " Completed Problem 05 from StacksAndQueues in C# Advanced module "

--- C# Advanced/StacksAndQueues/StacksAndQueuesExercise/05. FashionBoutique/Program.cs
+++ C# Advanced/StacksAndQueues/StacksAndQueuesExercise/05. FashionBoutique/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _05._FashionBoutique
 {
@@ -6,15 +8,38 @@ namespace _05._FashionBoutique
     {
         static void Main(string[] args)
         {
-            
+            Stack<int> clothes = new Stack<int>(Console.ReadLine()
+                                                       .Split(' ')
+                                                       .Select(int.Parse)
+                                                       .ToArray());
+            int capacity = int.Parse(Console.ReadLine());
 
+            int racks = 0;
+            int sum = 0;
 
+            while (clothes.Count > 0)
+            {
+                int currentCloth = clothes.Peek();
 
+                if (currentCloth + sum <= capacity)
+                {
+                    sum += currentCloth;
+                    clothes.Pop();
+                }
 
+                else
+                {
+                    sum = 0;
+                    racks += 1;
+                }
+            }
 
+            if (sum > 0)
+            {
+                racks += 1;
+            }
 
-
-
+            Console.WriteLine(racks);
         }
     }
 }
