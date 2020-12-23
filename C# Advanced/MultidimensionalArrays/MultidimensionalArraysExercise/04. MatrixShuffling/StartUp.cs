846d66a6c6e03d46c5560319f2474223462547e1
Teodor Yanchev <teo.yanchev93@gmail.com>
2020-12-24 00:32:29 +0200

    " completed 2 exercises from multidimensionArrays lecture "

---
+++ C# Advanced/MultidimensionalArrays/MultidimensionalArraysExercise/04. MatrixShuffling/StartUp.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._MatrixShuffling
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixDimensions = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rows = matrixDimensions[0];
+            int cols = matrixDimensions[1];
+
+            string[,] matrix = new string[rows, cols];
+
+            CreateMatrix(matrix);
+
+            string command = Console.ReadLine();
+            while (command != "END")
+            {
+                string[] currentCommand = command.Split(' ',
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                if (!(command.StartsWith("swap") && currentCommand.Length == 5))
+                {
+                    Console.WriteLine("Invalid input!");
+                    command = Console.ReadLine();
+                    continue;
+                }
+
+                int oldRow = int.Parse(currentCommand[1]);
+                int oldCol = int.Parse(currentCommand[2]);
+                int newRow = int.Parse(currentCommand[3]);
+                int newCol = int.Parse(currentCommand[4]);
+
+                bool oldIsInRange = (0 <= oldRow && oldRow < matrix.GetLength(0)) &&
+                                 (0 <= oldCol && oldCol < matrix.GetLength(1));
+                bool newIsInRange = (0 <= newRow && newRow < matrix.GetLength(0)) &&
+                                    (0 <= newCol && newCol < matrix.GetLength(1));
+
+                if (oldIsInRange && newIsInRange)
+                {
+                    string changeOld = matrix[oldRow, oldCol];
+                    string changeNew = matrix[newRow, newCol];
+
+                    matrix[oldRow, oldCol] = changeNew;
+                    matrix[newRow, newCol] = changeOld;
+
+                    PrintMatrix(matrix);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+
+        private static void PrintMatrix(string[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + ' ');
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void CreateMatrix(string[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] inputMatrix = Console.ReadLine().Split(' ',
+                    StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = inputMatrix[col];
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/MultidimensionalArrays/MultidimensionalArraysExercise/05. SnakeMoves/StartUp.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._SnakeMoves
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixDimension = Console.ReadLine().Split(' ',
+                StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = matrixDimension[0];
+            int cols = matrixDimension[1];
+            char[,] matrix = new char[rows, cols];
+
+            string input = Console.ReadLine();
+
+            int count = 0;
+            int startIndexRow = 0;
+            int countBreak = 0;
+
+
+            for (int row = startIndexRow; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (input.Length == count)
+                    {
+                        count = 0;
+                    }
+
+                    matrix[row, col] = input[count];
+                    count += 1;
+                }
+
+                row = startIndexRow + 1;
+
+                if (countBreak == (matrix.GetLength(0) / 2))
+                {
+                    break;
+                }
+
+                for (int reversedCol = matrix.GetLength(1) - 1; reversedCol >= 0; reversedCol--)
+                {
+                    if (input.Length == count)
+                    {
+                        count = 0;
+                    }
+
+                    matrix[row, reversedCol] = input[count];
+                    count += 1;
+                }
+
+                startIndexRow = row + 1;
+                countBreak += 1;
+            }
+
+            PrintMatrix(matrix);
+        }
+
+        private static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void CreateMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] inputMatrix = Console.ReadLine().Split(' ',
+                    StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = inputMatrix[col];
+                }
+            }
+        }
+    }
+}
+
