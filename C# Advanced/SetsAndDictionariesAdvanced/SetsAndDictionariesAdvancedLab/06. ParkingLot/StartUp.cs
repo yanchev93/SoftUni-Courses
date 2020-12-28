85ddc9440aa928f6702df35391224efcbfebeec1
Teodor Yanchev <teo.yanchev93@gmail.com>
2020-12-28 19:32:00 +0200

    " Finished SetsAndDictionariesAdvancedLab "

---
+++ C# Advanced/SetsAndDictionariesAdvanced/SetsAndDictionariesAdvancedLab/06. ParkingLot/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._ParkingLot
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> parkingLot = new HashSet<string>();
+
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                string[] currentInput = input.Split(", ",
+                    StringSplitOptions.RemoveEmptyEntries);
+                string direction = currentInput[0];
+                string carNumber = currentInput[1];
+
+                if (direction == "OUT" && parkingLot.Contains(carNumber))
+                {
+                    parkingLot.Remove(carNumber);
+                }
+
+                else if (direction == "IN")
+                {
+                    parkingLot.Add(carNumber);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (parkingLot.Count > 0)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, parkingLot));
+            }
+
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
---
+++ C# Advanced/SetsAndDictionariesAdvanced/SetsAndDictionariesAdvancedLab/07. SoftUniParty/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vipList = new HashSet<string>();
+            var regularList = new HashSet<string>();
+
+            string command = Console.ReadLine();
+            while (command != "PARTY")
+            {
+                int length = command.Length;
+
+                if (length == 8)
+                {
+                    if (char.IsDigit(command[0]))
+                    {
+                        vipList.Add(command);
+                    }
+                    else
+                    {
+                        regularList.Add(command);
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            command = Console.ReadLine();
+            while (command != "END")
+            {
+                if (vipList.Contains(command))
+                {
+                    vipList.Remove(command);
+                }
+
+                else if (regularList.Contains(command))
+                {
+                    regularList.Remove(command);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            int notShowedGuests = vipList.Count + regularList.Count;
+            Console.WriteLine(notShowedGuests);
+            if (vipList.Count > 0)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, vipList));
+            }
+
+            if (regularList.Count > 0)
+            {
+                Console.WriteLine(string.Join(Environment.NewLine, regularList));
+            }
+        }
+    }
+}
