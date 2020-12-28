b3dd789bc2b7cf20592794f61f5df3c7dc100daa
Teodor Yanchev <teo.yanchev93@gmail.com>
2020-12-28 21:52:29 +0200

    " completed case 6 from exercise "

--- C# Advanced/SetsAndDictionariesAdvanced/SetsAndDictionariesAdvancedExercise/06. Wardrobe/StartUp.cs
+++ C# Advanced/SetsAndDictionariesAdvanced/SetsAndDictionariesAdvancedExercise/06. Wardrobe/StartUp.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace _06._Wardrobe
 {
@@ -6,10 +8,60 @@ namespace _06._Wardrobe
     {
         static void Main(string[] args)
         {
+            var wardrobe = new Dictionary<string, Dictionary<string, int>>();
+            int n = int.Parse(Console.ReadLine());
 
+            for (int i = 0; i < n; i++)
+            {
+                string[] clothes = Console.ReadLine().Split(" -> ",
+                    StringSplitOptions.RemoveEmptyEntries);
+                string color = clothes[0];
+                string[] currentClothes = clothes[1].Split(',',
+                    StringSplitOptions.RemoveEmptyEntries);
 
+                if (!wardrobe.ContainsKey(color))
+                {
+                    wardrobe[color] = new Dictionary<string, int>();
+                }
 
+                var currentColor = wardrobe[color];
+                foreach (var cloth in currentClothes)
+                {
+                    if (!currentColor.ContainsKey(cloth))
+                    {
+                        currentColor[cloth] = 0;
+                    }
 
+                    currentColor[cloth] += 1;
+                }
+            }
+
+            string[] findCloth = Console.ReadLine().Split(' ',
+                StringSplitOptions.RemoveEmptyEntries);
+            string colorToFind = findCloth[0];
+            string clothToFind = findCloth[1];
+
+            var sbPrint = new StringBuilder();
+
+            foreach (var (color, clothes) in wardrobe)
+            {
+                sbPrint.AppendLine($"{color} clothes:");
+
+                foreach (var (cloth, total) in clothes)
+                {
+                    if (!(color == colorToFind && cloth == clothToFind))
+                    {
+                        sbPrint.AppendLine($"* {cloth} - {total}");
+                    }
+
+                    else
+                    {
+                        sbPrint.AppendLine($"* {cloth} - {total} (found!)");
+                    }
+                }
+            }
+
+            Console.WriteLine(sbPrint);
         }
     }
 }
