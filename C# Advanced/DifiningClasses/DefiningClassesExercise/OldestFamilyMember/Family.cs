fcff903a426ff36248a26ef8a4de0d27a7c20c55
Teodor Yanchev <teo.yanchev93@gmail.com>
2021-01-12 23:32:50 +0200

    " Completed case: 03. OldestFamilyMember from DefiningClassesExercise "

---
+++ C# Advanced/DifiningClasses/DefiningClassesExercise/OldestFamilyMember/Family.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> people;
+
+        public Family()
+        {
+            this.people = new List<Person>();
+        }
+        public List<Person> People
+        {
+            get { return this.people; }
+            set { this.people = value; }
+        }
+        public  void AddMember(Person member)
+        {
+            this.people.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            Person oldestPerson = people.First();
+
+            foreach (var member in people)
+            {
+                if (oldestPerson.Age < member.Age)
+                {
+                    oldestPerson = member;
+                }
+            }
+
+            return oldestPerson;
+        }
+    }
+}
---
+++ C# Advanced/DifiningClasses/DefiningClassesExercise/OldestFamilyMember/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var listFamily = new Family();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] person = Console.ReadLine().Split(' ',
+                    StringSplitOptions.RemoveEmptyEntries);
+                string name = person[0];
+                int age = int.Parse(person[1]);
+
+                Person currentPerson = new Person(name, age);
+
+                listFamily.AddMember(currentPerson);
+            }
+
+            var oldestMember = listFamily.GetOldestMember();
+
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+        }
+    }
+}
