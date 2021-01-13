d8734ee397756ede6735e3f03c290fd46cec7258
Teodor Yanchev <teo.yanchev93@gmail.com>
2021-01-13 23:03:23 +0200

    " extra push for case: 07. RawData "

---
+++ C# Advanced/DifiningClasses/DefiningClassesExercise/SpeedRacing/Tire.cs
@@ -0,0 +1,30 @@
+﻿namespace DefiningClasses
+{
+    public class Tire
+    {
+        // {tire1Pressure} {tire1Age} {tire2Pressure} {tire2Age} 
+        // {tire3Pressure} {tire3Age} {tire4Pressure} {tire4Age}
+
+        private int tireAge;
+        private double tirePressure;
+
+        public Tire(int tireAge, double tirePressure)
+        {
+            this.TireAge = tireAge;
+            this.TirePressure = tirePressure;
+        }
+
+        public int TireAge
+        {
+            get { return this.tireAge; }
+            set { this.tireAge = value; }
+        }
+
+        public double TirePressure
+        {
+            get { return this.tirePressure; }
+            set { this.tirePressure = value; }
+        }
+
+    }
+}
