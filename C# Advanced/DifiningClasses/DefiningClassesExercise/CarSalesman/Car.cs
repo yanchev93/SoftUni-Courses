41c878e0a8ecc457040cecb659cb1429ecbbe5aa
Teodor Yanchev <teo.yanchev93@gmail.com>
2021-01-15 23:10:20 +0200

    " Completed case: 08. CarSalesman from DefiningCLassesExercise "

---
+++ C# Advanced/DifiningClasses/DefiningClassesExercise/CarSalesman/Car.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        private string model;
+        private Engine engine;
+        private int? weight;
+        private string color;
+
+        public Car(string model, Engine engine)
+        {
+            this.Model = model;
+            this.Engine = engine;
+        }
+
+        public Car(string model, Engine engine, string color)
+            : this(model, engine)
+        {
+            this.Color = color;
+        }
+
+        public Car(string model, Engine engine, int weight)
+            : this(model, engine)
+        {
+            this.Weight = weight;
+        }
+
+        public Car(string model, Engine engine, int weight, string color)
+            : this(model, engine)
+        {
+            Weight = weight;
+            Color = color;
+        }
+
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+
+        public Engine Engine
+        {
+            get { return this.engine; }
+            set { this.engine = value; }
+        }
+
+        public int? Weight
+        {
+            get { return this.weight; }
+            set { this.weight = value; }
+        }
+
+        public string Color
+        {
+            get { return this.color; }
+            set { this.color = value; }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string weightStr = this.Weight.HasValue ?
+                this.Weight.ToString() : "n/a";
+            string colorStr = String.IsNullOrEmpty(this.Color) ? "n/a"
+                : this.Color;
+
+            sb
+                .AppendLine($"{this.Model}:")
+                .AppendLine($"{this.Engine}")
+                .AppendLine($"  Weight: {weightStr}")
+                .AppendLine($"  Color: {colorStr}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# Advanced/DifiningClasses/DefiningClassesExercise/CarSalesman/StartUp.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            List<Engine> engines = new List<Engine>();
+            List<Car> cars = new List<Car>();
+
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] engineInfo = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                InputDataEngine(engineInfo, engines);
+            }
+
+            int m = int.Parse(Console.ReadLine());
+            for (int i = 0; i < m; i++)
+            {
+                string[] carInfo = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                InputDataCar(carInfo, cars, engines);
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, cars));
+        }
+
+        private static void InputDataCar(string[] info, List<Car> cars, List<Engine> engines)
+        {
+            string model = info[0];
+            string engine = info[1];
+            Engine findEngine = engines.Find(p => p.Model == engine);
+
+            if(info.Length == 2)
+            {
+                Car car = new Car(model, findEngine);
+
+                cars.Add(car);
+            }
+            else if (info.Length == 3)
+            {
+                int weight;
+
+                bool isWeight = int.TryParse(info[2], out weight);
+
+                if (isWeight)
+                {
+                    weight = int.Parse(info[2]);
+
+                    Car car = new Car(model, findEngine, weight);
+
+                    cars.Add(car);
+                }
+                else
+                {
+                    string color = info[2];
+
+                    Car car = new Car(model, findEngine, color);
+
+                    cars.Add(car);
+                }
+            }
+            else if (info.Length == 4)
+            {
+                int weight = int.Parse(info[2]);
+                string color = info[3];
+
+                Car car = new Car(model, findEngine, weight, color);
+
+                cars.Add(car);
+            }
+        }
+
+        private static void InputDataEngine(string[] info, List<Engine> engines)
+        {
+            string model = info[0];
+            int power = int.Parse(info[1]);
+
+            if (info.Length == 2)
+            {
+                Engine engine = new Engine(model, power);
+
+                engines.Add(engine);
+            }
+            else if (info.Length == 3)
+            {
+                int displacement;
+
+                bool isDisplacement = int.TryParse(info[2], out displacement);
+
+                if (isDisplacement)
+                {
+                    Engine engine = new Engine(model, power, displacement);
+
+                    engines.Add(engine);
+                }
+                else
+                {
+                    string efficiency = info[2];
+
+                    Engine engine = new Engine(model, power, efficiency);
+
+                    engines.Add(engine);
+                }
+            }
+            else if (info.Length == 4)
+            {
+                int displacement = int.Parse(info[2]);
+                string efficiency = info[3];
+
+                Engine engine = new Engine(model, power, displacement, efficiency);
+
+                engines.Add(engine);
+            }
+        }
+    }
+}
