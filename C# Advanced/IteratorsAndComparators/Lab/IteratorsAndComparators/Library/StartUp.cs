6cf470c3d2c7085fcf476cb87cc145bca295beeb
Teodor Yanchev <teo.yanchev93@gmail.com>
2021-01-20 23:31:25 +0200

    " Completed Case 01: Library from IteratorsAndComparators "

---
+++ C# Advanced/IteratorsAndComparators/Lab/IteratorsAndComparators/Library/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Book bookOne = new Book("Animal Farm", 2003, new List<string> { "Geaorge Orwell" });
+            Book bookTwo = new Book("The Documents in the Case", 2002, new List<string> { "Dorothy Sayers", "Robert Eustace" });
+            Book bookThree = new Book("The Documents in the Case", 1930, new List<string> { });
+
+            Library libraryOne = new Library();
+            Library libraryTwo = new Library(bookOne, bookTwo, bookThree);
+
+            ;
+        }
+    }
+}
