88679cce5414bf4362e210ebb3652c038da2bcdd
Teodor Yanchev <teo.yanchev93@gmail.com>
2021-02-03 20:54:20 +0200

    " Completed case:5 Restaurant from C# OOP InheritanceExercise "

--- C# OOP/InheritanceExercise/Restaurant/StartUp.cs
+++ C# OOP/InheritanceExercise/Restaurant/StartUp.cs
@@ -1,9 +1,21 @@
 ﻿namespace Restaurant
 {
+    using System;
+
     public class StartUp
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
+            Product coldBeverage = new ColdBeverage("Icecream Shake", 5.50m, 450);
+            Product coffee = new Coffee("Double Espresso", 2);
+
+            Console.WriteLine(coldBeverage.Name);
+            Console.WriteLine(coffee.Name);
+            Console.WriteLine(coldBeverage.Name);
+            Console.WriteLine(coldBeverage.Name);
+            Console.WriteLine(coldBeverage.Name);
+
+
 
         }
     }
