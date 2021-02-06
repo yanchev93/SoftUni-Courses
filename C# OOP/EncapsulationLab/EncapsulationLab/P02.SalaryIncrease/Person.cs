800e28e0b22eb2afd7099f876ef2b312240cbfa3
Teodor Yanchev <teo.yanchev93@gmail.com>
2021-02-06 19:40:20 +0200

    " Completed case:2 and 3 from C# OOP EncapsulationLab "

---
+++ C# OOP/EncapsulationLab/EncapsulationLab/P02.SalaryIncrease/Person.cs
@@ -0,0 +1,79 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName,int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            private set
+            {
+                this.firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            private set
+            {
+                this.lastName = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            private set
+            {
+                this.age = value;
+            }
+        }
+        public decimal Salary
+        {
+            get
+            {
+                return this.salary;
+            }
+            private set
+            {
+                this.salary = value;
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            
+            if (this.Age < 30)
+            {
+                percentage /= 2;
+            }
+
+            this.Salary += this.Salary * percentage / 100;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.salary:f2} leva.";
+        }
+    }
+}
