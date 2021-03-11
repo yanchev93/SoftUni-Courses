aecf99f3d418f98e601319aa5082a72aea8a5544
Teodor Yanchev <teo.yanchev93@gmail.com>
2021-03-11 23:37:17 +0200

    Completed P02.ValidationAttributes from C# OOP ReflectionAndAttributes

---
+++ C# OOP/ReflectionAndAttributes/ReflectionAndAttributesExercise/ValidationAttributes/MyRangeAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationAttributes
+{
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private int minAge;
+        private int maxAge;
+
+        public MyRangeAttribute(int minAge, int maxAge)
+        {
+            this.minAge = minAge;
+            this.maxAge = maxAge;
+        }
+
+        public override bool IsValid(object obj)
+        {
+            if (obj is int valueAsInt)
+            {
+                if (valueAsInt >= minAge && valueAsInt <= maxAge)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            throw new ArgumentException("Invalid type!");
+        }
+    }
+}
---
+++ C# OOP/ReflectionAndAttributes/ReflectionAndAttributesExercise/ValidationAttributes/Validator.cs
@@ -0,0 +1,35 @@
+﻿
+namespace ValidationAttributes
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            PropertyInfo[] objProperties = obj.GetType().GetProperties();
+
+            foreach (var propertyInfo in objProperties)
+            {
+                IEnumerable<MyValidationAttribute> propAttributes = propertyInfo
+                    .GetCustomAttributes()
+                    .Where(a => a is MyValidationAttribute)
+                    .Cast<MyValidationAttribute>();
+
+                foreach (var propAttribute in propAttributes)
+                {
+                    bool result = propAttribute.IsValid(propertyInfo.GetValue(obj));
+
+                    if (!result)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
