08e832a13118880c364003ed3f5b7752bf3e9795
Teodor Yanchev <teo.yanchev93@gmail.com>
2021-03-11 19:12:28 +0200

    Completed P01.CommandPattern from C# OOP ReflectionAndAttributes

---
+++ C# OOP/ReflectionAndAttributes/ReflectionAndAttributesExercise/CommandPattern/Commands/ExitCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace CommandPattern.Commands
+{
+    using System;
+    using CommandPattern.Core.Contracts;
+    public class ExitCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            Environment.Exit(0);
+            return null;
+        }
+    }
+}
---
+++ C# OOP/ReflectionAndAttributes/ReflectionAndAttributesExercise/CommandPattern/Commands/HelloCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace CommandPattern.Commands
+{
+    using CommandPattern.Core.Contracts;
+    public class HelloCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            string name = args[0];
+
+            return $"Hello, {name}";
+        }
+    }
+}
