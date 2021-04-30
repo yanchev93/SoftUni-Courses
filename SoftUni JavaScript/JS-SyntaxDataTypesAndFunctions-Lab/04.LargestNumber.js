f458505c8ab73a611e5c40c26e64c1bbf7d4c454
Teodor Yanchev <teo.yanchev93@gmail.com>
2021-05-01 01:25:01 +0300

    JS - Objects Lab

--- SoftUni JavaScript/JS-SyntaxDataTypesAndFunctions-Lab/04.LargestNumber.js
+++
@@ -1,8 +0,0 @@
-function solve(firstNum, secondNum, thirdNum){
-    let maxNum = Math.max(firstNum, secondNum, thirdNum);
-
-    console.log(`The largest number is ${maxNum}.`);
-}
-
-solve(5, -3, 16)
-solve(-3, -5, -22.5)
