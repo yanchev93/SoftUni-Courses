f23ff73c410d8df67e1e999ff7a44ae649a5864a
Teodor Yanchev <teo.yanchev93@gmail.com>
2021-05-13 23:20:13 +0300

    JS Advanced

--- SoftUni JavaScript/05-JS-DOMManipulation-Lab/02. Add-Delete/app.js
+++ SoftUni JavaScript/05-JS-DOMManipulation-Lab/02. Add-Delete/app.js
@@ -1,3 +1,24 @@
 function addItem() {
-    //TODO...
+    const input = document.querySelector('#newItemText');
+    const liElement = createElement('li', input.value);
+
+
+    const deleteBtn = createElement('a', '[Delete]');
+    deleteBtn.href = '#';
+    deleteBtn.addEventListener('click', onClick);
+
+    liElement.appendChild(deleteBtn);
+
+    document.querySelector('#items').appendChild(liElement);
+    input.value = '';
+
+    function onClick(event) {
+        event.target.parentNode.remove();
+    }
+
+    function createElement(type, content) {
+        const element = document.createElement(type);
+        element.textContent = content;
+        return element;
+    }
 }
--- SoftUni JavaScript/05-JS-DOMManipulation-Lab/05. Highlight-Active/app.js
+++ SoftUni JavaScript/05-JS-DOMManipulation-Lab/05. Highlight-Active/app.js
@@ -1,3 +1,19 @@
 function focused() {
-    console.log('TODO:...');
+    const input = document.querySelectorAll('input');
+
+    Array.from(input).forEach(i => {
+        i.addEventListener('focus', onFocus);
+        i.addEventListener('blur', onBlur)
+    });
+
+    function onFocus(event) {
+        // console.log('focused');
+        event.target.parentNode.className = 'focused';
+    }
+
+    function onBlur(event) {
+        // console.log('blurred');
+        event.target.parentNode.className = '';
+    }
+
 }
