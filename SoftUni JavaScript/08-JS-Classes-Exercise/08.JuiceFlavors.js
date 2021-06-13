38cb18c3e084233a0e8d20cd83486c3888224f58
Teodor Yanchev <teo.yanchev93@gmail.com>
2021-06-13 15:22:35 +0300

    JS Advanced - Classes Exercise

---
+++ SoftUni JavaScript/08-JS-Classes-Exercise/08.JuiceFlavors.js
@@ -0,0 +1,17 @@
+function createJuice(input) {
+    let juiceBottles = {};
+
+    let arr = input
+        .map(x => x.split(' => '))
+        
+
+
+    console.log(output);
+}
+
+createJuice(['Orange => 2000',
+    'Peach => 1432',
+    'Banana => 450',
+    'Peach => 600',
+    'Strawberry => 549']
+);
