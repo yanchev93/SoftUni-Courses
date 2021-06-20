f0d36d96600aafd6bed9ec4032d05d2d913a3ca4
Teodor Yanchev <teo.yanchev93@gmail.com>
2021-06-20 18:26:54 +0300

    JS Exam

--- SoftUni Exams/JS-Advanced/JS Advanced - Exam Prep 1 - 12.02.2021/Skeleton/index.js
+++ SoftUni Exams/JS-Advanced/JS Advanced - Exam Prep 1 - 12.02.2021/Skeleton/index.js
@@ -1,11 +1,13 @@
 function solve() {
     const addBtn = document.querySelector('button');
+    const delBtn = document.querySelector('.red') == null ? undefined : document.querySelector('.red');
 
     const lectureName = document.querySelector('input[name="lecture-name"]');
     const lectureDate = document.getElementsByName('lecture-date')[0];
     const lectureModule = document.getElementsByTagName('select')[0];
 
     let trainings = document.querySelector('.modules');
+    let objModules = {};
 
     addBtn.addEventListener('click', (e) => {
         e.preventDefault();
@@ -14,58 +16,126 @@ function solve() {
             return;
         }
 
+        let liElement;
+        let btnElement;
+        let outputElement;
+        let title;
+
+        let name;
+        let date;
+
+        let ulElement = createEl('ul');
+
+        let divH3 = Array.from(document.querySelectorAll('.module h3'));
+
         let addTitle = createEl('h3', lectureModule.value);
         addTitle.textContent = addTitle.textContent.toUpperCase() + '-MODULE';
-        let ulElement = document.createElement('ul');
-        let liElement = document.createElement('li');
-        let outputElement = document.createElement('h4');
-        let btnElement = createEl('button', 'Del', 'red');
 
-        const currentModules = document.querySelectorAll('.modules>h3');
-        let allModules = [];
-        if (currentModules.length > 0) {
-            currentModules.forEach(x => allModules.push(x.textContent));
-        }
+        let doExist = divH3.filter(x => x.textContent == addTitle.textContent);
+        if (doExist.length > 0) {
+            const parentDiv = doExist[0].parentNode;
+            title = addTitle.textContent;
+            name = lectureName.value;
+            date = lectureDate.value.split('-').join('/').split('T').join(' - ');
 
-        if (allModules.includes(addTitle.textContent)) {
-            console.log('HUI');
-        }
+            objModules[title].push({ name, date });
+            objModules[title].sort((a, b) => a.date.localeCompare(b.date));
 
+            let liArr = [];
+            for (const data of objModules[title]) {
+                let [name, date] = Object.values(data);
+                liElement = createEl('li', undefined, 'flex');
+                btnElement = createEl('button', 'Del', 'red');
+                outputElement = createEl('h4');
 
-        trainings.appendChild(addTitle);
+                outputElement.textContent = name + ' - ' + date;
 
-
-        let date = lectureDate.value;
-        let result = [];
-        for (let i = 0; i < date.length; i++) {
-            let letter = date[i];
-            if (letter == '-') {
-                letter = '/';
-            } else if (letter == 'T') {
-                letter = ' - ';
+                liElement.appendChild(outputElement);
+                liElement.appendChild(btnElement);
+                liArr.push(liElement);
             }
 
-            result.push(letter);
+            parentDiv.removeChild(parentDiv.lastChild);
+            liArr.forEach(x => ulElement.appendChild(x));
+            parentDiv.appendChild(ulElement);
+
+        } else {
+            title = addTitle.textContent;
+            name = lectureName.value;
+            date = lectureDate.value.split('-').join('/').split('T').join(' - ');
+
+            if (!objModules[title]) {
+                objModules[title] = [];
+            }
+
+            objModules[title].push({ name, date });
+
+            for (const data of objModules[title]) {
+                let [name, date] = Object.values(data);
+                liElement = createEl('li', undefined, 'flex');
+                btnElement = createEl('button', 'Del', 'red');
+                outputElement = createEl('h4');
+
+                outputElement.textContent = name + ' - ' + date;
+
+                liElement.appendChild(outputElement);
+                liElement.appendChild(btnElement);
+                ulElement.appendChild(liElement);
+
+            }
+
+
+            let divModule = createEl('div', undefined, 'module');
+            divModule.appendChild(addTitle);
+            divModule.appendChild(ulElement);
+            trainings.appendChild(divModule);
         }
+    });
 
-        const output = lectureName.value + ' - ' + result.join('');
-        outputElement.textContent = output;
-        liElement.className = 'flex';
-        liElement.appendChild(outputElement);
-        liElement.appendChild(btnElement);
-        ulElement.appendChild(liElement);
-        trainings.appendChild(ulElement);
+    document.body.addEventListener('click', function (e) {
+        if (e.target.className == 'red') {
+            let parentLi = e.target.parentNode;
 
+            const parentDiv = parentLi.parentNode.parentNode;
+            const h3 = parentLi.parentNode.parentNode.children[0];
 
+            let h4 = parentLi.querySelector('h4');
+            let dataModule = parentLi.parentNode.parentNode.firstChild;
+            let objData = Object.entries(objModules).find(x => x[0] == dataModule.textContent);
+            let parentUl = Array.from(parentLi.parentNode.children);
+            let deleteUl = parentLi.parentNode;
 
+            let index;
+            parentUl.forEach((x, i) => {
+                if (x.firstChild.textContent == h4.textContent) {
+                    index = i;
+                }
+            });
+
+            objData[1].splice(index, 1);
+            parentLi.remove();
+
+            let lengthArr = parentUl.length - 1;
+
+            if (lengthArr == 0) {
+                deleteUl.remove();
+
+                parentDiv.remove();
+                delete objModules[objData[0]];
+            }
+        }
     });
 
     function createEl(type, content, className) {
         const result = document.createElement(type);
-        result.textContent = content;
+        if (content) {
+            result.textContent = content;
+        }
+
         if (className) {
             result.className = className;
         }
+
         return result;
     }
 
