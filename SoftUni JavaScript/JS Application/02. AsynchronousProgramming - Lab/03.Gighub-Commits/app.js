3c89bf58061c8656620e65a48d9eeff9fae3239b
Teodor Yanchev <teo.yanchev93@gmail.com>
2021-07-04 13:39:09 +0300

    JS Application

--- SoftUni JavaScript/JS Application/02. AsynchronousProgramming - Lab/03.Gighub-Commits/app.js
+++ SoftUni JavaScript/JS Application/02. AsynchronousProgramming - Lab/03.Gighub-Commits/app.js
@@ -1,4 +1,27 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+    const username = document.getElementById('username').value;
+    const repo = document.getElementById('repo').value;
+    const ulElement = document.getElementById('commits');
+    ulElement.innerHTML = '';
+
+    const url = `https://api.github.com/repos/${username}/${repo}/commits`;
+
+    const response = await fetch(url);
+
+    if (response.ok) {
+        const data = await response.json();
+
+        data.forEach(r => {
+            console.log(r.commit.author.name);
+            const liElement = document.createElement('li');
+            liElement.textContent = `${r.commit.author.name}: ${r.commit.message}`;
+
+            ulElement.appendChild(liElement);
+        });
+    } else if (response.status = '404') {
+        const liElement = document.createElement('li');
+        liElement.textContent = `Error: ${response.status} (Not Found)`;
+
+        ulElement.appendChild(liElement);
+    }
 }
