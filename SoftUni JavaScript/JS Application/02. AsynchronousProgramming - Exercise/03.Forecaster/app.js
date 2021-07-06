5a72cc9d291ac3adb62406532f46af9522b4ab28
Teodor Yanchev <teo.yanchev93@gmail.com>
2021-07-07 00:38:15 +0300

    JS Application

--- SoftUni JavaScript/JS Application/02. AsynchronousProgramming - Exercise/03.Forecaster/app.js
+++ SoftUni JavaScript/JS Application/02. AsynchronousProgramming - Exercise/03.Forecaster/app.js
@@ -6,7 +6,21 @@ async function attachEvents(ev) {
 attachEvents();
 
 async function getWeather() {
+    const currentElement = document.querySelector('#current');
+    const upcomingElement = document.querySelector('#upcoming');
+    upcomingElement.style.display = 'block';
+
+    currentElement.innerHTML = '';
+    upcomingElement.innerHTML = '';
+
+    const currentLabel = e('div', { className: 'label', innerHTML: 'Current conditions' });
+    currentElement.appendChild(currentLabel);
+
+    const upcomingLabel = e('div', { className: 'label', innerHTML: 'Three-day forecast' });
+    upcomingElement.appendChild(upcomingLabel);
+
     const cityName = document.getElementById('location').value;
+    document.querySelector("#forecast").style.display = 'block';
 
     const symbols = {
         'Sunny': '&#x2600;',
@@ -24,10 +38,32 @@ async function getWeather() {
             upcomingData(code)
         ]);
 
+        const divCurrent = e('div', { className: 'forecast' },
+            e('span', { className: 'condition symbol', innerHTML: `${symbols[today.forecast.condition]}` }),
+            e('span', { className: 'condition' },
+                e('span', { className: 'forecast-data', innerHTML: `${today.name}` }),
+                e('span', { className: 'forecast-data', innerHTML: `${today.forecast.high}${symbols.Degrees}/${today.forecast.low}${symbols.Degrees}` }),
+                e('span', { className: 'forecast-data', innerHTML: `${today.forecast.condition}` })
+            ),
+        );
 
+        currentElement.appendChild(divCurrent);
 
-    } catch (error) {
+        const divUpForecast = e('div', { className: 'forecast-info' });
+        upcoming.forecast.forEach(x => {
+            let divUpcomming =
+                e('span', { className: 'upcoming' },
+                    e('span', { className: 'symbol', innerHTML: `${symbols[x.condition]}` }),
+                    e('span', { className: 'forecast-data', innerHTML: `${x.high}${symbols.Degrees}/${x.low}${symbols.Degrees}` }),
+                    e('span', { className: 'forecast-data', innerHTML: `${x.condition}` })
+                );
+            divUpForecast.appendChild(divUpcomming);
+        });
 
+        upcomingElement.appendChild(divUpForecast);
+    } catch (err) {
+        upcomingElement.style.display = 'none';
+        currentElement.children[0].innerHTML = 'Error';
     }
 
 }
