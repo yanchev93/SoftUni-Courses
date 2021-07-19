97524380fecc880f3fab2798bb593d1388c247ff
Teodor Yanchev <teo.yanchev93@gmail.com>
2021-07-20 00:04:13 +0300

    JS application

--- SoftUni JavaScript/JS Application/06_ClientSideRendering_Exercise/03.Search-in-List/search.js
+++ SoftUni JavaScript/JS Application/06_ClientSideRendering_Exercise/03.Search-in-List/search.js
@@ -1,3 +1,32 @@
+import { render } from './../node_modules/lit-html/lit-html.js';
+import { towns } from './towns.js';
+import { allListTemplate } from './templates/listTemplate.js';
+
+const townsDiv = document.getElementById('towns');
+const inputField = document.getElementById('searchText');
+
+let updatedTowns = [];
+towns.map(t => {
+   updatedTowns.push({ name: t, class: undefined });
+});
+
+render(allListTemplate(updatedTowns), townsDiv);
+
+
+document.querySelector('button').addEventListener('click', search);
 function search() {
-   // TODO
+   const inputValue = inputField.value.toLowerCase();
+   if (inputValue == '') {
+      return;
+   }
+
+   updatedTowns.forEach(t => {
+      if (t.name.toLowerCase().includes(inputValue)) {
+         t.class = 'active';
+      } else {
+         t.class = undefined;
+      }
+   });
+
+   render(allListTemplate(updatedTowns), townsDiv);
 }
